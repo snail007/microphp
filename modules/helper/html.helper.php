5d7164eaabddc982c6ea804a0b80b441a5859c55
snail <arraykeys@gmail.com>
2013-04-27 15:04:15 +0800

    Signed-off-by:snail <arraykeys@gmail.com>

--- modules/helper/html.helper.php
+++ modules/helper/html.helper.php
@@ -2,16 +2,23 @@
 if (!function_exists('enableSelectDefault')) {
     function enableSelectDefault($return = false) {
         $js = '<script>
-                var selects=document.getElementsByTagName("select");
-                for(var k=0;k<selects.length;k++){
-                    var s=selects[k];
-                    var defaultv=s.attributes["default"]?s.attributes["default"].value:null;
-                    if(defaultv){
-                        for(var i=0;i<s.length;i++){
-                        console.log(s[i].value);
-                            if(s[i].value==defaultv){
-                            s[i].selected=true;
-                            break;
+                var func0797986876; 
+                if(typeof(window.onload)=="function"){
+                  func0797986876=window.onload;
+                }
+                window.onload=function(){
+                    func0797986876?func0797986876():null;
+                    var selects=document.getElementsByTagName("select");
+                    for(var k=0;k<selects.length;k++){
+                        var s=selects[k];
+                        var defaultv=s.attributes["default"]?s.attributes["default"].value:null;
+                        if(defaultv){
+                            for(var i=0;i<s.length;i++){
+                            console.log(s[i].value);
+                                if(s[i].value==defaultv){
+                                s[i].selected=true;
+                                break;
+                                }
                             }
                         }
                     }
