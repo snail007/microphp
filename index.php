35169e3946bcd44769d74abf3ae850d081bfd69a
snail <arraykeys@gmail.com>
2013-07-03 10:05:46 +0800

    Signed-off-by:snail <arraykeys@gmail.com>

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
  * @copyright          Copyright (c) 2013 - 2013, 狂奔的蜗牛, Inc.
  * @link		https://bitbucket.org/snail/microphp/
  * @since		Version 2.0
- * @createdtime       2013-06-21 09:38:03
+ * @createdtime       2013-07-03 02:05:39
  */
 define('IN_WONIU_APP', TRUE);
 //------------------------system config----------------------------
