f96efd4c7dc8d111ede73f0518eacc67b6d32871
snail <arraykeys@gmail.com>
2013-07-29 11:09:06 +0800

    Signed-off-by:snail <arraykeys@gmail.com>

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
  * @copyright          Copyright (c) 2013 - 2013, 狂奔的蜗牛, Inc.
  * @link		http://git.oschina.net/snail/microphp
  * @since		Version 2.1.4
- * @createdtime       2013-07-26 16:03:57
+ * @createdtime       2013-07-26 17:19:28
  */
 define('IN_WONIU_APP', TRUE);
 define('WDS', DIRECTORY_SEPARATOR);
