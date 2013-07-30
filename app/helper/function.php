901e7a7834c33a9b4a09a99d44151a3f5aa86085
snail <arraykeys@gmail.com>
2013-07-30 10:16:41 +0800

    Signed-off-by: snail <arraykeys@gmail.com>

--- app/helper/function.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-function testFunction($param) {
-    echo $param;
-}
--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include('MicroPHP.plugin.php');
-WoniuController::instance('home.TestHook')->doTest();
-echo '<br/>';
-WoniuModel::instance('User2')->printHook();
-echo '<br/>';
-WoniuModel::instance('test.User')->sayHello('snail');
-WoniuLoader::instance()->view('snail'); 
