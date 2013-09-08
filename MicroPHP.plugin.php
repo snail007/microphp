0d25eaa2d90a9421ca1b1934735fadf5a57a5021
snail <arraykeys@gmail.com>
2013-09-08 11:59:14 +0800

    Signed-off-by: snail <arraykeys@gmail.com>

--- MicroPHP.plugin.php
+++ MicroPHP.plugin.php
@@ -11,7 +11,7 @@
  * @copyright          Copyright (c) 2013 - 2013, 狂奔的蜗牛, Inc.
  * @link		http://git.oschina.net/snail/microphp
  * @since		Version 2.1.9
- * @createdtime       2013-09-08 11:04:08
+ * @createdtime       2013-09-08 11:58:21
  */
 define('IN_WONIU_APP', TRUE);
 define('WDS', DIRECTORY_SEPARATOR);
