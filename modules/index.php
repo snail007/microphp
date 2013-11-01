4e12e1937163f5dc7d553afe6c5afa708180d2bf
snail <arraykeys@gmail.com>
2013-11-01 09:32:41 +0800

    Merge development
    Conflicts:
    	docs/index_ver.html

--- modules/index.php
+++ modules/index.php
@@ -44,29 +44,29 @@ $system['route'] = array(
     "/^welcome\\/?(.*)$/u" => 'welcome.ajax/$1'
 );
 /**
- * Caching Config
+ * 缓存配置
  */
 $system['cache_drivers'] = array();
 $system['cache_config'] = array(
     /*
-     * Default storage
-     * if you set this storage => "files", then $cache = phpFastCache(); <-- will be files cache
-     * auto orders : apc,sqlite,files,memcached,wincache, xcache,memcache
+     * 默认存储方式
+     * 可用的方式有：auto,apc,sqlite,files,memcached,wincache, xcache,memcache
+     * auto自动模式寻找的顺序是 : apc,sqlite,files,memcached,wincache, xcache,memcache
      */
-    "storage" => "auto", // auto,apc,sqlite,files,memcached,wincache, xcache,memcache
+    "storage" => "auto",
+    /*
+     * 默认缓存文件存储的路径
+     * 使用绝对全路径，比如： /home/username/cache
+     * 留空，系统自己选择
+     */
+    "path" => "", // 缓存文件存储默认路径
+    "securityKey" => "", // 缓存安全key，建议留空，系统会自动处理 PATH/securityKey
 
     /*
-     * Default Path for Cache on HDD
-     * Use full PATH like /home/username/cache
-     * Keep it blank "", it will automatic setup for you
-     */
-    "path" => "", // default path for files
-    "securityKey" => "", // default will good. It will create a path by PATH/securityKey
-
-    /*
-     * FallBack Driver
-     * Example, in your code, you use memcached, apc..etc, but when you moved your web hosting
-     * The new hosting don't have memcached, or apc. What you do? Set fallback that driver to other driver.
+     * 第二驱动
+     * 比如：当你现在在代码中使用的是memcached, apc等等，然后你的代码转移到了一个新的服务器而且不支持memcached 或 apc
+     * 这时候怎么办呢？设置第二驱动即可，当你设置的驱动不支持的时候，系统就使用第二驱动。
+     * $key是你设置的驱动，当设置的“storage”=$key不可用时，就使用$key对应的$value驱动
      */
     "fallback" => array(
         "memcache" => "files",
@@ -77,12 +77,12 @@ $system['cache_config'] = array(
         "sqlite" => "files",
     ),
     /*
-     * .htaccess protect
-     * default will be  true
+     * .htaccess 保护
+     * true会自动在缓存文件夹里面建立.htaccess文件防止web非法访问
      */
     "htaccess" => false,
     /*
-     * Default Memcache Server for all $cache = phpFastCache("memcache");
+     * 所有通过$cache = phpFastCache("memcache")创建的对象使用的Memcache服务器地址;
      */
     "server" => array(
         array("127.0.0.1", 11211, 1),
