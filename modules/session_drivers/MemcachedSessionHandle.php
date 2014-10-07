bf16c7f0831292d91b1545ed9225c0ceb97fa6d8
snail <arraykeys@gmail.com>
2014-10-07 22:17:42 +0800

    Signed-off-by: snail <arraykeys@gmail.com>

--- modules/session_drivers/MemcachedSessionHandle.php
+++
@@ -1,7 +0,0 @@
-<?php
-class MemcachedSessionHandle {
-    public function start($config = array()) {
-        ini_set('session.save_handler', 'memcached');
-        ini_set('session.save_path', $config['memcached']);
-    }
-}
